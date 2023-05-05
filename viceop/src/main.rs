173499353c390c914ee74bc053ea75081cd639a0
John Wregglesworth <wregglej@cyverse.org>
2023-05-05 10:53:10 -0700

    Add crate for viceop

---
+++ viceop/src/main.rs
@@ -0,0 +1,32 @@
+use futures::{StreamExt, TryStreamExt};
+use k8s_openapi::api::apps::v1::Deployment;
+use k8s_openapi::api::core::v1::{ConfigMap, Service};
+use k8s_openapi::api::networking::v1::Ingress;
+use kube::api::ListParams;
+use kube::{
+    api::{Api, DeleteParams, Patch, PatchParams, ResourceExt},
+    Client,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::init();
+    let kube_client = Client::try_default().await?;
+    let config_maps: Api<ConfigMap> = Api::namespaced(kube_client.clone(), "viceapps");
+    let ingresses: Api<Ingress> = Api::namespaced(kube_client.clone(), "viceapps");
+    let deployments: Api<Deployment> = Api::namespaced(kube_client.clone(), "viceapps");
+
+    for cm in config_maps.list(&ListParams::default()).await? {
+        println!("configmap: {}", cm.name_any());
+    }
+
+    for i in ingresses.list(&ListParams::default()).await? {
+        println!("ingress: {}", i.name_any());
+    }
+
+    for d in deployments.list(&ListParams::default()).await? {
+        println!("deployment: {}", d.name_any());
+    }
+
+    Ok(())
+}
